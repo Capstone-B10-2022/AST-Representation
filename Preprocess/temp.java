d53e14a037e526b75056a34a61733fb5fe994732
NikhilAdyapak <nikhiladyapak31@gmail.com>
2022-09-18 18:45:41 +0530

    create

---
+++ Preprocess/temp.java
@@ -0,0 +1,11 @@
+
+class ON_2 {
+ 
+    public static void main(String[] args)
+    {
+        int i, n = 8;
+        for (i = 1; i <= n; i++) {
+            System.out.printf("Hello World !!!\n");
+        }
+    }
+}
